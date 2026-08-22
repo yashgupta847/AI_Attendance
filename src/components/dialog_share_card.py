8df0eaa120d420c93169ed547da773365a134c3c
Yash Gupta <Yash Gupta>
2026-08-22 12:09:21 +0530

    Update domain

--- src/components/dialog_share_card.py
+++ src/components/dialog_share_card.py
@@ -3,7 +3,7 @@ import segno
 import io
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "1aisnapclass-main.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
     st.header("Scan to Join")
     qr = segno.make(join_url)
